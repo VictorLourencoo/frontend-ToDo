b2ca7c6ea38c96393bc1431b8599f82eeb6ac593
victorlourencoo <victorlourenco443@gmail.com>
2020-05-28 11:57:41 -0300

    Rotas

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from '../views/Home/index';
+import Task from '../views/Task/index';
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/task" exact component={Task} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
